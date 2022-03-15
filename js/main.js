e2d6e4dd36c32baff8cfbdebafd56201ebc000db
Felix Odete <felix.odette@gmail.com>
2022-03-15 16:02:11 +0300

    merged branch

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ let menuIcon = document.querySelector(".menu-icon");
 let sideBar = document.querySelector(".side-bar");
 let videos = document.querySelector(".videos");
 
-menuIcon.onlick = function () {
+menuIcon.onClick = function () {
   sideBar.classList.toggle("no-side-bar");
   videos.classList.toggle("large-videos");
 }
+
+
